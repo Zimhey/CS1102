bfbb85a92a3b0c604fb7316daae8a77d2abf2b6c
Maryann O'Connell <maryann.oc91@gmail.com>
2015-09-07 13:55:53 -0400

    HW2 Outline

--- HW2.rkt
+++ HW2.rkt
@@ -1,4 +1,78 @@
 ;; The first three lines of this file were inserted by DrRacket. They record metadata
 ;; about the language level of this file in a form that our tools can easily process.
-#reader(lib "htdp-beginner-reader.ss" "lang")((modname HW2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
-"Test"
+#reader(lib "htdp-beginner-abbr-reader.ss" "lang")((modname HW2) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #t)))
+;;Corey Dixon
+;;Maryann O'Connell
+;;CS 1102 (A15)
+
+;;Homework 2: The Fire Fighter Game
+
+;;DATA DEFINITION FOR A "world"
+
+;;a world is ...
+
+;;Examples of worlds
+
+
+
+
+;;DATA DEFINTIONS TO WRITE "world" DATA DEFINTION
+;;(included templates for all data definitions)
+
+;;a ___ is...
+
+;;Examples of ______
+
+
+
+
+;;FUNCTION "draw-world"
+
+;;draw-world:
+;;consumes a ...
+;;and produces a...
+
+;;Definition
+
+
+;;Test cases
+
+
+
+;;FUNCTION "update-world"
+
+;;update-world:
+;;consumes a ...
+;;and produces a...
+
+;;Definition
+
+
+;;Test cases
+
+
+
+
+;;FUNCTION "process-keys"
+
+;;process-keys:
+;;consumes a ...
+;;and produces a...
+
+;;Definition
+
+
+;;Test cases
+
+
+
+
+;;BUILT-IN FUNCTIONS FOR GAME PLAY
+;;(INITWORLD is instance of the world datatype)
+
+#|
+(big-bang INITWORLD
+          (on-draw draw-world)
+          (on-tick update-world)
+          (on-key process-keys))
+|#
